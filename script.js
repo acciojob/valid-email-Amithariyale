718eec41c3003bb70af231c70da96a866215ace1
Amit Hariyale <57282097+Amithariyale@users.noreply.github.com>
2023-12-12 16:15:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 function validEmail(str) {
   //your JS code here.
+	str=str.trim();
+	if(str.length==0)return false;
+	if(str[0]=='@')return false;
+	if(!str.includes('@'))return false;
+	if(!str.includes('.com') && !str.includes('.co.in') && !str.includes('.edu'))return false;
+	return true;
 }
 
 // Do not change the code below.
 const str = prompt("Enter an email address.");
 alert(validEmail(str));
+
+
+
+
